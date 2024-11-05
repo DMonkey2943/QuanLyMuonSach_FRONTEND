9445363b44b2e6cb906eaa4f0ffea5aff8021e42
DMonkey2943 <ltnduyen29042003@gmail.com>
2024-11-05 22:45:27 +0700

    Add BooksView and handle BorrowBook for Reader

---
+++ src/components/Reader/BookList.vue
@@ -0,0 +1,68 @@
+<script>
+import BorrowBookService from '@/services/borrowBook.service'
+
+export default {
+  props: {
+    books: { type: Array },
+    activeIndex: { type: Number, default: -1 }
+  },
+  emits: ['update:activeIndex'],
+  methods: {
+    updateActiveIndex(index) {
+      this.$emit('update:activeIndex', index)
+    },
+    async borrowBook(book) {
+      try {
+        const data = {
+          DocGiaId: localStorage.getItem('id'),
+          SachId: book._id
+        }
+        await BorrowBookService.createBorrowBook(data)
+        alert(`Bạn đã mượn sách "${book.TenSach}"!`)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <table class="table table-bordered table-hover">
+    <thead>
+      <tr>
+        <th scope="col">STT</th>
+        <th scope="col">Tên Sách</th>
+        <th scope="col">Hình Ảnh</th>
+        <th scope="col">Tác giả</th>
+        <th scope="col">Nhà xuất bản</th>
+        <th scope="col">Năm xuất bản</th>
+        <th scope="col">Số quyển</th>
+        <th scope="col">Đơn giá</th>
+        <th scope="col">Tùy chọn</th>
+      </tr>
+    </thead>
+    <tbody v-for="(book, index) in books" :key="book._id">
+      <tr>
+        <td>{{ index + 1 }}</td>
+        <td scope="row">{{ book.TenSach }}</td>
+        <td>
+          <img
+            v-if="book.HinhAnh"
+            :src="book.HinhAnh"
+            alt="Hình ảnh sách"
+            style="max-width: 80px"
+          />
+        </td>
+        <td>{{ book.TacGia }}</td>
+        <td>{{ book.NhaXuatBan.TenNXB }}</td>
+        <td>{{ book.NamXuatBan }}</td>
+        <td>{{ book.SoQuyen }}</td>
+        <td>{{ book.DonGia }}</td>
+        <td>
+          <button type="button" class="btn btn-success" @click="borrowBook(book)">Mượn sách</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
---
+++ src/views/Reader/BooksView.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="row">
+    <div class="mt-3">
+      <h4>
+        Mượn sách
+        <i class="fas fa-book"></i>
+      </h4>
+
+      <div class="m-3 row justify-content-around-align-items-center">
+        <button class="btn btn-sm btn-primary" @click="refreshList">
+          <i class="fas fa-redo"></i>Làm mới
+        </button>
+      </div>
+
+      <BookList
+        v-if="filteredBooksCount > 0"
+        :books="filteredBooks"
+        v-model:activeIndex="activeIndex"
+      />
+      <p v-else>Không có Sách nào.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+// import BookCard from '@/components/BookCard.vue'
+import BookList from '@/components/Reader/BookList.vue'
+import BookService from '@/services/book.service'
+
+export default {
+  components: {
+    // BookCard,
+    BookList
+  },
+  data() {
+    return {
+      books: [],
+      activeIndex: -1
+    }
+  },
+  watch: {},
+  computed: {
+    // Chuyen cac doi tuong Contact thanh chuoi de tien cho tim kiem:
+    bookStrings() {
+      return this.books.map((book) => {
+        const { TenSach, TacGia, DonGia, SoQuyen, NamXuatBan, HinhAnh, NhaXuatBan } = book
+        return [TenSach, TacGia, DonGia, SoQuyen, NamXuatBan, HinhAnh, NhaXuatBan].join('')
+      })
+    },
+    // Tra ve cac Contact co chua thong tin can tim kiem:
+    filteredBooks() {
+      return this.books
+    },
+    activeBook() {
+      if (this.activeIndex < 0) return null
+      console.log('Clicked')
+      return this.filteredBooks[this.activeIndex]
+    },
+    filteredBooksCount() {
+      return this.filteredBooks.length
+    }
+  },
+  methods: {
+    async retrieveBooks() {
+      try {
+        this.books = await BookService.getAllBooks()
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    refreshList() {
+      this.retrieveBooks()
+      this.activeIndex = -1
+    },
+    goToAddBook() {
+      this.$router.push({
+        name: 'book.add'
+      })
+    }
+  },
+  mounted() {
+    this.refreshList()
+  }
+}
+</script>
+
+<style>
+.page {
+  text-align: left;
+  max-width: 750px;
+}
+</style>
