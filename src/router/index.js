578c6ac2aff2d3c6c264c77da19ec31d59dc139c
DMonkey2943 <ltnduyen29042003@gmail.com>
2024-11-16 15:33:29 +0700

    Add check role before access route

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,7 @@ const routes = [
     path: '/',
     name: 'dashboard',
     component: Dashboard,
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, allowedRoles: ['admin', 'nhanvien', 'docgia'] }
   },
   {
     path: '/login',
@@ -37,109 +37,109 @@ const routes = [
     path: '/reader/books',
     name: 'reader.books',
     component: ReaderBooks,
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, allowedRoles: ['docgia'] }
   },
   {
     path: '/reader/borrowBooks',
     name: 'reader.borrowBooks',
     component: ReaderBorrowBooks,
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, allowedRoles: ['docgia'] }
   },
   {
     path: '/books',
     name: 'books',
     component: Books,
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, allowedRoles: ['admin', 'nhanvien'] }
   },
   {
     path: '/book/:id',
     name: 'book.edit',
     component: () => import('@/views/BookEdit.vue'),
     props: true, //Truyền các biến trong $route.params vào làm props
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, allowedRoles: ['admin', 'nhanvien'] }
   },
   {
     path: '/book/add',
     name: 'book.add',
     component: () => import('@/views/BookAdd.vue'),
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, allowedRoles: ['admin', 'nhanvien'] }
   },
   {
     path: '/publishers',
     name: 'publishers',
     component: Publishers,
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, allowedRoles: ['admin', 'nhanvien'] }
   },
   {
     path: '/publisher/:id',
     name: 'publisher.edit',
     component: () => import('@/views/PublisherEdit.vue'),
     props: true, //Truyền các biến trong $route.params vào làm props
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, allowedRoles: ['admin', 'nhanvien'] }
   },
   {
     path: '/publisher/add',
     name: 'publisher.add',
     component: () => import('@/views/PublisherAdd.vue'),
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, allowedRoles: ['admin', 'nhanvien'] }
   },
   {
     path: '/employees',
     name: 'employees',
     component: Employees,
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, allowedRoles: ['admin'] }
   },
   {
     path: '/employee/:id',
     name: 'employee.edit',
     component: () => import('@/views/EmployeeEdit.vue'),
     props: true, //Truyền các biến trong $route.params vào làm props
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, allowedRoles: ['admin'] }
   },
   {
     path: '/employee/add',
     name: 'employee.add',
     component: () => import('@/views/EmployeeAdd.vue'),
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, allowedRoles: ['admin'] }
   },
   {
     path: '/readers',
     name: 'readers',
     component: Readers,
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, allowedRoles: ['admin', 'nhanvien'] }
   },
   {
     path: '/reader/:id',
     name: 'reader.edit',
     component: () => import('@/views/ReaderEdit.vue'),
     props: true, //Truyền các biến trong $route.params vào làm props
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, allowedRoles: ['admin', 'nhanvien'] }
   },
   {
     path: '/reader/add',
     name: 'reader.add',
     component: () => import('@/views/ReaderAdd.vue'),
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, allowedRoles: ['admin', 'nhanvien'] }
   },
   {
     path: '/borrowBooks',
     name: 'borrowBooks',
     component: BorrowBooks,
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, allowedRoles: ['admin', 'nhanvien'] }
   },
   {
     path: '/borrowBooks/book/:id',
     name: 'borrowBooksByBook',
     component: () => import('@/views/BorrowBookByBookView.vue'),
     props: true, //Truyền các biến trong $route.params vào làm props
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, allowedRoles: ['admin', 'nhanvien'] }
   },
   {
     path: '/borrowBooks/reader/:id',
     name: 'borrowBooksByReader',
     component: () => import('@/views/BorrowBookByReaderView.vue'),
     props: true, //Truyền các biến trong $route.params vào làm props
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, allowedRoles: ['admin', 'nhanvien'] }
   },
   {
     path: '/:pathMatch(.*)*',
@@ -155,11 +155,12 @@ const router = createRouter({
 
 // Global navigation guard
 router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+  const role = localStorage.getItem('role') // Lấy vai trò từ localStorage
+
   // Kiểm tra xem route có yêu cầu đăng nhập không
   if (to.matched.some((record) => record.meta.requiresAuth)) {
     // Kiểm tra xem token có tồn tại không
-    const token = localStorage.getItem('token')
-
     if (!token) {
       // Nếu không có token, chuyển hướng về trang đăng nhập
       next({
@@ -167,8 +168,15 @@ router.beforeEach((to, from, next) => {
         query: { redirect: to.fullPath } // Chuyển hướng người dùng về trang login và lưu lại trang đã yêu cầu
       })
     } else {
-      // Nếu có token, cho phép tiếp tục vào route
-      next()
+      // Nếu có token, kiểm tra vai trò
+      const allowedRoles = to.meta.allowedRoles || [] // Lấy danh sách vai trò được phép
+      if (allowedRoles.length && !allowedRoles.includes(role)) {
+        // Nếu vai trò không được phép truy cập
+        alert('Bạn không có quyền truy cập vào trang này.')
+        next({ name: 'dashboard' }) // Chuyển hướng về dashboard hoặc trang khác phù hợp
+      } else {
+        next() // Vai trò hợp lệ, cho phép truy cập
+      }
     }
   } else {
     // Nếu route không yêu cầu đăng nhập, cho phép tiếp tục
